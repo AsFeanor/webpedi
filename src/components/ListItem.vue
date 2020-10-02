4c977c31593ace29e6a5c4889d62d95bcab844fd
Arda <arda@Arda-MacBook-Pro.local>
2020-10-02 21:35:43 +0300

    some fix

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="ListItem">
+  <div class="ListItem" v-if="data">
     <div
       class="row no-gutters overflow-hidden flex-md-row mb-5  h-md-250 position-relative"
     >
@@ -10,37 +10,28 @@
           class="bd-placeholder-img"
           width="275"
           height="200"
-          preserveAspectRatio="xMidYMid slice"
-          focusable="false"
           role="img"
           aria-label="Placeholder: Thumbnail"
           style="border-radius: 4%"
         />
       </div>
       <div class="col p-4 d-flex flex-column position-static">
-        <a
-          href="../../public/img/turk-silahlari-almanyada-kullanilacak.jpg"
-          style="color: #333;"
-          ><h4 class="mb-3">{{ data.title }}</h4></a
-        >
+        <router-link :to="`/detail/${data.id}`">
+          <h4 class="mb-3">{{ data.title }}</h4>
+        </router-link>
         <p class="mb-auto text-muted" style="font-size: .85rem;">
           {{ data.description }}
         </p>
-        <a
-          href="#"
-          class="btn p-link text-right"
-          style="font-size: 11px; font-weight: 600;"
-          >DEVAMINI OKU→</a
-        >
+        <router-link :to="`/detail/${data.id}`">
+          <h4 class="mb-3">Devamını oku</h4>
+        </router-link>
       </div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  props:{
-    data: Object
-  },
+  props:['data'],
   name: "ListItem",
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import Blog1 from "../views/Blog1.vue";
 
 Vue.use(VueRouter);
 
@@ -12,9 +11,9 @@ const routes = [
     component: Home
   },
   {
-    path: "/blog",
-    name: "Blog1",
-    component: Blog1
+    path: "/detail/:id",
+    name: "BlogDetail",
+    component: () => import('../components/BlogDetail.vue'),
   }
 ];
 
